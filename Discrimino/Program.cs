f153ca4ac715076dce86b50a62cac33b274ab822
eugbor <eugeneborodin@mail.ru>
2016-08-27 03:34:36 +0300

    commit

--- Discrimino/Program.cs
+++ Discrimino/Program.cs
@@ -9,23 +9,31 @@ namespace Discrimino
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine()) ;
             double c = double.Parse(Console.ReadLine());
-            double p = 2;
-            if (!(a == 0))
+
+            if (a == 0) Console.WriteLine("'а' не может быть равно 0");
+            else
             {
-                double d = (Math.Pow(b, p)) - (4 * a * c);
-                if (d >= 0)
-                {
-                    if (d > 0)
-                    {
-                        Console.WriteLine("Квадратное уравнение имеет два различных действительных корня: " +
-                      "x1 = {0:0.00}, x2 = {1:0.00}.", (((-b) + Math.Sqrt(d)) / (2 * a)), (((-b) - Math.Sqrt(d)) / (2 * a)));
-                    }
-                    else Console.WriteLine("Квадратное уравнение имеет два совпадающих корня: x1 = x2 = {0}.", ((-b) / (2*a)));
-                }
-                else Console.WriteLine("Квадратное уравнение не имеет действительных корней.");
+                double d = Math.Pow(b, 2) - (4 * a * c);
+
+                if (!(d >= 0))
+                    Console.WriteLine("Квадратное уравнение не имеет действительных корней.");
+                else if (!(d > 0))
+                    Console.WriteLine("Квадратное уравнение имеет два совпадающих корня: x1 = x2 = {0:0.00}.", (-b / (2 * a)));
+                else
+                    Console.WriteLine("Квадратное уравнение имеет два различных действительных корня: " +
+                    "x1 = {0:0.00}, x2 = {1:0.00}.", ((-b + Math.Sqrt(d)) / (2 * a)), ((-b - Math.Sqrt(d)) / (2 * a)));
+                 
+
+                //double d = Math.Pow(b, 2) - (4*a*c);
+                //while (true)
+                //{
+                //    if (!(d >= 0)) { Console.WriteLine("Квадратное уравнение не имеет действительных корней."); break; }
+                //    if (!(d > 0))  { Console.WriteLine("Квадратное уравнение имеет два совпадающих корня: " +
+                //    "x1 = x2 = {0:0.00}.", (-b / (2 * a))); break;}
+                //    Console.WriteLine("Квадратное уравнение имеет два различных действительных корня: " +
+                //    "x1 = {0:0.00}, x2 = {1:0.00}.", ((-b + Math.Sqrt(d)) / (2 * a)), ((-b - Math.Sqrt(d)) / (2 * a))); break;
+                //}
             }
-            else Console.WriteLine("'а' не может быть равно 0");
-            
             Console.ReadLine();
         }
     }
